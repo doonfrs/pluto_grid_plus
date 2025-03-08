2d5022b5ac3f318416c6ec1cf0e6da0dc196b1aa
Feras abdalrahman <doonfrs@gmail.com>
2025-03-09 01:18:07 +0300

    � Deploy updated Flutter Web Demo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3640913619"
+    serviceWorkerVersion: "3535405792"
   }
 });
