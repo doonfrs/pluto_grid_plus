6acb542bc6d171fed114c20c7910d0a1e3188b6d
Feras abdalrahman <doonfrs@gmail.com>
2025-03-08 21:03:22 +0300

    udpate deploy script

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -6,39 +6,33 @@ BRANCH="gh-pages"
 DEMO_DIR="demo"
 BUILD_DIR="$DEMO_DIR/build/web"
 
-echo "🚀 Starting Flutter Web Build Process from '$DEMO_DIR'..."
+echo "🚀 Building Flutter Web Demo..."
 
 git checkout master
 git pull origin master
 
 cd $DEMO_DIR
-
 flutter pub get
-flutter build web --release
-
+flutter build web --release --base-href=/pluto_grid_plus/
 cd ..
 
-echo "✅ Flutter Web Build Completed!"
+echo "✅ Build Completed!"
+
 if git rev-parse --verify $BRANCH >/dev/null 2>&1; then
     git checkout $BRANCH
+    git pull origin $BRANCH
 else
     git checkout --orphan $BRANCH
-    git commit --allow-empty -m "Initial commit for GitHub Pages"
 fi
 
-echo "🧹 Cleaning up old files while keeping .git..."
 git rm -rf . >/dev/null 2>&1
-
-echo "🔁 Moving demo build files to the repository root..."
 cp -r $BUILD_DIR/* .
 cp -r $BUILD_DIR/. .
-rm -rf demo
 
 git add .
 git commit -m "🚀 Deploy updated Flutter Web Demo"
-
-git push -f origin $BRANCH
+git push origin $BRANCH # No force push unless necessary!
 
 git checkout master
 
-echo "🎉 Deployment completed!"
+echo "🎉 Deployment Completed!"
